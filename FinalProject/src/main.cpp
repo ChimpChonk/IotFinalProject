e2fd9b292bc034c80f79d5d3dd6f3ef3cee75ef3
ChimpChonk <sanjit.poudel28@gmail.com>
2024-06-02 22:21:35 +0200

    wip

--- FinalProject/src/main.cpp
+++ FinalProject/src/main.cpp
@@ -2,7 +2,7 @@
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
 #include <AsyncTCP.h>
-#include <FS.h>
+#include "FS.h"
 #include <LittleFS.h>
 //AsyncWebServer port
 AsyncWebServer server(80);
@@ -11,7 +11,7 @@ AsyncWebServer server(80);
 const char* PARAM_INPUT_1 = "ssid";
 const char* PARAM_INPUT_2 = "pass";
 const char* PARAM_INPUT_3 = "ip";
-const char* PARAm_INPUT_4 = "gateway";
+const char* PARAM_INPUT_4 = "gateway";
 
 //Var save values for HTML form
 String ssid;
@@ -23,8 +23,8 @@ String gateway;
 //File path save for wifi config values
 const char* ssidPath = "/ssid.txt";
 const char* passPath = "/pass.txt";
-const char* ipPaht = "/ip.txt";
-const char* gatewayPath = "gateway.txt"; 
+const char* ipPath = "/ip.txt";
+const char* gatewayPath = "/gateway.txt"; 
 
 //local IP
 IPAddress localIP;
@@ -32,6 +32,9 @@ IPAddress localIP;
 //local gaveway
 IPAddress localGateway;
 IPAddress subnet(255, 255, 0, 0);
+
+unsigned long previousMillis = 0;
+const long interval = 10000;
 
 //init littleFS
 void initLittleFS()
@@ -45,10 +48,149 @@ void initLittleFS()
   Serial.println("Mounting success");
 }
 
+//Reading file from littleFS
+String readFile(fs::FS &fs, const char * path){
+  Serial.printf("Reading file: %s\r\n", path);
+
+  File file = fs.open(path);
+  if(!file || file.isDirectory()){
+    Serial.println("- failed to open file for reading");
+    return String();
+  }
+
+  String fileContent;
+  while(file.available()){
+    fileContent = file.readStringUntil('\n');
+  }
+  return fileContent;
+}
+
+//Writing file to littleFS
+void writeFile(fs::FS &fs, const char * path, const char * message){
+  Serial.printf("Writing file: %s\r\n", path);
+
+  File file = fs.open(path, FILE_WRITE);
+  if(!file){
+    Serial.println("- failed to open file for writing");
+    return;
+  }
+  if(file.print(message)){
+    Serial.println("- file written");
+  } else {
+    Serial.println("- write failed");
+  }
+}
+
+//wifi init
+bool initWiFi(){
+  if(ssid == "" || ip == ""){
+    Serial.println("Undefined SSID or IP");
+    return false;
+  }
+
+  WiFi.mode(WIFI_STA);
+  localIP.fromString(ip.c_str());
+  localGateway.fromString(gateway.c_str());
+
+  if(!WiFi.config(localIP, localGateway, subnet)){
+    Serial.println("Failed to configure");
+    return false;
+  }
+
+  WiFi.begin(ssid.c_str(), pass.c_str());
+  Serial.println("Connecting to WiFi...");
+
+  unsigned long currentMillis = millis();
+  while(WiFi.status() != WL_CONNECTED){
+    if(millis() - currentMillis >= interval){
+      Serial.println("Failed to connect");
+      return false;
+    }
+  }
+
+  Serial.println(WiFi.localIP());
+  return true;
+}
+
 void setup() {
   Serial.begin(115200);
   initLittleFS();
-  // put your setup code here, to run once:
+
+  ssid = readFile(LittleFS, ssidPath);
+  pass = readFile(LittleFS, passPath);
+  ip = readFile(LittleFS, ipPath);
+  gateway = readFile(LittleFS, gatewayPath);
+  Serial.println(ssid);
+  Serial.println(ip);
+  Serial.println(gateway);
+
+  if(initWiFi()){
+    Serial.println("HTTP server started");
+    server.begin();
+  }
+    else {
+    // Connect to Wi-Fi network with SSID and password
+    Serial.println("Setting AP (Access Point)");
+    // NULL sets an open Access Point
+    WiFi.softAP("Ape-ESP-AP", NULL);
+
+    IPAddress IP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(IP); 
+
+    // Web Server Root URL
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+      request->send(LittleFS, "/wifimanager.html", "text/html");
+    });
+    
+    server.serveStatic("/", LittleFS, "/");
+    
+    server.on("/", HTTP_POST, [](AsyncWebServerRequest *request) {
+      int params = request->params();
+      for(int i=0;i<params;i++){
+        AsyncWebParameter* p = request->getParam(i);
+        if(p->isPost()){
+          // HTTP POST ssid value
+          if (p->name() == PARAM_INPUT_1) {
+            ssid = p->value().c_str();
+            Serial.print("SSID set to: ");
+            Serial.println(ssid);
+            // Write file to save value
+            writeFile(LittleFS, ssidPath, ssid.c_str());
+          }
+          // HTTP POST pass value
+          if (p->name() == PARAM_INPUT_2) {
+            pass = p->value().c_str();
+            Serial.print("Password set to: ");
+            Serial.println(pass);
+            // Write file to save value
+            writeFile(LittleFS, passPath, pass.c_str());
+          }
+          // HTTP POST ip value
+          if (p->name() == PARAM_INPUT_3) {
+            ip = p->value().c_str();
+            Serial.print("IP Address set to: ");
+            Serial.println(ip);
+            // Write file to save value
+            writeFile(LittleFS, ipPath, ip.c_str());
+          }
+          // HTTP POST gateway value
+          if (p->name() == PARAM_INPUT_4) {
+            gateway = p->value().c_str();
+            Serial.print("Gateway set to: ");
+            Serial.println(gateway);
+            // Write file to save value
+            writeFile(LittleFS, gatewayPath, gateway.c_str());
+          }
+          //Serial.printf("POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
+        }
+      }
+      request->send(200, "text/plain", "Done. ESP will restart, connect to your router and go to IP address: " + ip);
+      delay(3000);
+      ESP.restart();
+    });
+    server.begin();
+  }
 }
 
 void loop() {
